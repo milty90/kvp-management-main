474ed9e8b83e60c2d0c8ac49d714f4bf39051c91
milantyopity <milant1990@gmail.com>
2026-04-27 21:54:16 +0200

    enhance ProfileModal layout and add user information

--- src/components/items/ProfileModal.tsx
+++ src/components/items/ProfileModal.tsx
@@ -17,14 +17,17 @@ export function ProfileModal({ onConfirm, onCancel }: ProfileModalProps) {
   const { theme } = useTheme();
   const width = useWindowWidth();
 
+  const department = "Entwicklung";
+  const role = "Testbenutzer";
+
   useEffect(() => {
     fetchUser(setUsername, setEmail);
   }, []);
 
   return (
     <div className="fixed z-50 inset-0 flex items-center justify-center bg-gray-700/50">
-      <div className="bg-surface px-6 pt-8 md:py-5 md:rounded-xl shadow-lg w-full h-full md:h-auto max-w-xl relative">
-        <h2 className="text-xl font-bold pl-1 mb-4">Profil</h2>
+      <div className="bg-surface px-6 pt-6 md:py-5 md:rounded-xl shadow-lg w-full h-full md:h-auto max-w-xl relative">
+        <h2 className="text-xl font-bold pl-1">Profil</h2>
         <button
           type="button"
           className="absolute top-4 right-7 text-3xl text-gray-500 hover:text-text-primary"
@@ -36,11 +39,11 @@ export function ProfileModal({ onConfirm, onCancel }: ProfileModalProps) {
         <div className="border-t border-border my-4"></div>
 
         <div className="flex flex-col items-start justify-start mb-2 p-5 rounded-lg border border-gray-500">
-          <div className="flex flex-row w-full justify-between items-center px-3">
+          <div className="flex flex-row w-full justify-between items-center px-1">
             <img
               src="/face-id.png"
               alt="Profilbild"
-              className="w-24 h-24 rounded-md object-cover pl-1"
+              className="w-24 h-24 rounded-md object-cover"
             />
             <ColorButton
               color="gray"
@@ -50,47 +53,52 @@ export function ProfileModal({ onConfirm, onCancel }: ProfileModalProps) {
                   width,
                   theme,
                   "info",
-                  "Profilbild ändern ist derzeit nicht verfügbar.",
+                  "Profil ändern ist derzeit nicht verfügbar.",
                 )
               }
             >
               Profil ändern
             </ColorButton>
           </div>
-          <div className="flex flex-col items-start justify-start mt-3 px-4 gap-1">
+          <div className="flex flex-col items-start justify-start mt-3 px-1 gap-1">
             <p className=" text-text-primary text-xl font-semibold">
               Benutzername: {username}
             </p>
-            <p className="text-sm text-gray-500">Weitere Informationen</p>
+            <p className="text-sm text-gray-500">Weitere Informationen...</p>
           </div>
         </div>
-        <div className="flex items-center justify-around p-5 h-30 mb-2 rounded-lg border border-dashed border-gray-300">
-          <div className="text-sm text-gray-500">
-            <p className="text-2xl">12</p>
-            <p>Aktiv</p>
+        <div className="flex items-center justify-between  h-20 mb-2 gap-2 ">
+          <div className="flex flex-col items-center text-sm w-3/12 lg:w-4/12 text-gray-500 border border-gray-500 rounded-lg px-5 py-3">
+            <p className="text-3xl text-blue-500">12</p>
+            <p className="text-md text-text-secondary ">Aktiv</p>
           </div>
-          <div className="text-sm text-gray-500">
-            <p className="text-2xl">12</p>
-            <p>Zugewiesen</p>
+          <div className="flex flex-col items-center text-sm w-4/12 lg:w-4/12 text-gray-500 border border-gray-500 rounded-lg px-5 py-3">
+            <p className="text-3xl text-yellow-500">5</p>
+            <p className="text-md text-text-secondary ">Zugewiesen</p>
           </div>
-          <div className="text-sm text-gray-500">
-            <p className="text-2xl">12</p>
-            <p>Done</p>
+          <div className="flex flex-col items-center text-sm w-5/13 lg:w-4/12 text-gray-500 border border-gray-500 rounded-lg px-5 py-3">
+            <p className="text-3xl text-green-500">1</p>
+            <p className="text-md text-text-secondary ">Abgeschlossen</p>
           </div>
         </div>
-        <div className="flex flex-col items-start justify-center px-8 py-6 mb-2 rounded-lg border border-dashed border-gray-300">
-          <p className="text-md text-gray-500 my-1.5">E-Mail: {email}</p>
-          <div className="border-t border-0 border-border w-full mb-2 px-2"></div>
-          <p className="text-md text-gray-500 my-1.5">Abteilung: </p>
-          <div className="border-t border-0 border-border w-full mb-2 px-2"></div>
-          <p className="text-md text-gray-500 my-1.5">Rolle: </p>
-          <div className="border-t border-0 border-border w-full mb-2 px-2"></div>
-          <p className="text-md text-gray-500 my-1.5">Weitere Informationen</p>
+        <div className="flex flex-col items-start justify-center px-6 lg:px-8 py-3 mb-2 rounded-lg border border-gray-500">
+          <p className="text-md text-text-secondary my-1">
+            E-Mail: <span className="text-text-primary pl-3">{email}</span>
+          </p>
+          <p className="text-md text-text-secondary my-1">
+            Abteilung:{" "}
+            <span className="text-text-primary pl-3">{department}</span>
+          </p>
+          <p className="text-md text-text-secondary my-1">
+            Rolle: <span className="text-text-primary pl-3">{role}</span>
+          </p>
+          <p className="text-md text-text-secondary my-1">
+            Letzte Anmeldung:
+            <span className="text-text-primary pl-3">23.03.2026</span>
+          </p>
         </div>
-        <div className="flex flex-col items-start justify-center px-8 py-6 mb-2 rounded-lg border border-dashed border-gray-300">
-          <p className="text-md text-gray-500 my-1.5">Weitere Informationen</p>
-        </div>
-        <div className="mt-4 flex justify-end gap-3">
+        <div className="border-t border-border my-4"></div>
+        <div className="mt-4 pr-2 flex justify-end gap-3">
           <button
             onClick={onCancel}
             className={`px-4 py-2 rounded-md text-sm font-medium ${theme === "dark" ? " text-text-secondary hover:text-text-primary hover:bg-gray-700/80" : "text-text-secondary hover:text-text-primary hover:bg-gray-300/80"} transition-colors duration-150`}
