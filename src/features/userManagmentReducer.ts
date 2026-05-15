9f36dd78f8350264562e75658484facd24a0c73e
milantyopity <milant1990@gmail.com>
2026-05-15 14:34:46 +0200

    refactor userManagmentReducer to simplify state updates

--- src/features/userManagmentReducer.ts
+++ src/features/userManagmentReducer.ts
@@ -4,21 +4,18 @@ export default function userManagmentReducer(
   state: UserManagementState,
   action: UserManagementAction,
 ) {
-  let updatedState: UserManagementState;
 
-  switch (action.type) {
+ switch (action.type) {
     case "ADD_USER":
-      return (updatedState = [...state, action.user]);
-
+      return [...state, action.user];
     case "UPDATE_USER":
-      return (updatedState = state.map((user) =>
+      return state.map((user) =>
         user.userId === action.user.userId ? action.user : user,
-      ));
-
+      );
     case "DELETE_USER":
       return state.filter((user) => user.userId !== action.userId);
     case "SET_USERS":
-      return (updatedState = action.users);
+      return action.users;
     default:
       return state;
   }
